d2200030040f2a0a88c8d14a43d117e232e8121e
SeoHyeokGyu <shk64099@gmail.com>
2020-04-29 19:17:12 +0900

    ch08-coreML  commit

--- ch08-CoreML/ViewController.swift
+++ ch08-CoreML/ViewController.swift
@@ -7,14 +7,63 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
-class ViewController: UIViewController {
+class ViewController: UIViewController{
+    
+    var imagePickerController = UIImagePickerController()
+    
 
+    @IBOutlet weak var imageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        imagePickerController.delegate = self
         // Do any additional setup after loading the view.
     }
+    @IBAction func cameraButtonClicked(_ sender: UIBarButtonItem) {
+        if UIImagePickerController.isSourceTypeAvailable(.camera){
+            imagePickerController.sourceType = .camera
+        } else {
+            imagePickerController.sourceType = .photoLibrary
+        }
+        present(imagePickerController, animated: true)
+    }
+    
 
+}
+extension ViewController: UINavigationControllerDelegate, UIImagePickerControllerDelegate{
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+          let image = info[UIImagePickerController.InfoKey.originalImage] as! UIImage
+        
+        imageView.image = image
+        
+        imagePickerController.dismiss(animated: true, completion: nil)
+        
+    }
+    
+    func detectImage(image: CIImage){
+        
+        guard let model = try? VNCoreMLModel(for: Inceptionv3().model) else{
+        fatalError("Loading CoreML model failed")
+        }
+        let request = VNCoreMLRequest(model: model) { (request, error) in
+            guard let results = request.results as? [VNClassificationObservation] else{
+                fatalError("Model failed to process image")
+                }
+            if let topResult = results.first{
+                if topResult.identifier.contains("hotdog"){
+                    self.navigationItem.title = "Hotdog"
+                }else{
+                    self.navigationItem.title = "Not Hotdog"
+                    
+                }
+                
+            }
+        }
+    }
 
+  
 }
 
