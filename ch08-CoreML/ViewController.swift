20f8184c52fa2e58a55fe85d8e7d409254634baf
SeoHyeokGyu <shk64099@gmail.com>
2020-04-29 18:35:18 +0900

    Initial Commit

---
+++ ch08-CoreML/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ch08-CoreML
+//
+//  Created by 서혁규 on 2020/04/29.
+//  Copyright © 2020 서혁규. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
